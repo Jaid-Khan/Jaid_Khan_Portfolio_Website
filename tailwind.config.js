b7cfa6e545762d9094349398a9440d4c20b5369c
Jaid-Khan <jaidkhan04072001@gmail.com>
2024-11-30 20:48:10 +0530

    Development Done

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      // Custom Shadow 
+      dropShadow: {
+        'character-shadow': '2px 2px 20px #000000', // X-axis, Y-axis, blur, color
+      },
+
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'], // Define your custom font here
+      },
+    },
+  },
+  plugins: [],
+}
+
