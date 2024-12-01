4082af2ed948b9d265070d4f9af335625274555a
Jaid-Khan <jaidkhan04072001@gmail.com>
2024-12-01 17:26:30 +0530

    Deleted Unneccessary Items

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
+  css: ["./src/output.css"],
   theme: {
     extend: {
       // Custom Shadow 
